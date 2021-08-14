bf6fe3141c0019e4b1c878d89d18650e3cbd9478
dhr231 <dhruvprashar231@gmail.com>
2021-08-14 21:19:17 +0530

    ADD : Cart Overview Card Component using React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import BookStoreHome from './component/bookStore-home/bookStore-home';
 import  {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import CartPage from './component/Cart/CartPage';
 function App() {
   return (
     <div className="App">
@@ -7,6 +8,9 @@ function App() {
         <Switch>
           <Route exact path='/'>
             <BookStoreHome/>
+          </Route>
+          <Route path='/cart'>
+            <CartPage/>
           </Route>
         </Switch>
       </Router>
---
+++ src/component/Cart/CartPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './CartPage.scss';
+function CartPage() {
+    return (
+        <div>
+
+        <div id="cart">
+            <h4 id='cart-title'>My Cart(2)</h4>
+            <img className='book-cover' src="https://kbimages1-a.akamaihd.net/c2cddc29-ddd4-41d7-bb02-c89981b7c652/1200/1200/False/think-and-grow-rich-1937-original-masterpiece-by-napoleon-hill-2015-02-05.jpg" alt="" />
+                <div id='book-info'>
+                 <h4 className='book-title'>Think And Grow Rich</h4>
+                    <p className='author-name'>By Napolean Hill</p>
+                    <h4>Rs.1500</h4>
+                        <span className='controls'>
+                            <div id="quantity">
+                                 <button disabled={true}>-</button>
+                                 <input type="text" defaultValue={1} contentEditable={false}/>
+                                 <button>+</button>
+                            </div>
+                         <h6>Remove</h6>
+                        </span>
+                </div>
+                <button id="place-order">PLACE ORDER</button>
+                
+        </div>
+
+        </div>
+    )
+}
+
+export default CartPage;
