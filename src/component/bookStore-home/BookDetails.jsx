f662a569febfe4c2e35f098501373d554e4f2baf
Sonali Bhardwaj <66784132+Sonali-25@users.noreply.github.com>
2021-08-16 19:20:02 +0530

    FEAT: Filter_Functionality

--- src/component/bookStore-home/BookDetails.jsx
+++ src/component/bookStore-home/BookDetails.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import Card from './Card';
-import './bookStore-home.scss';
+import Bdata from './Bdata';
+import './BookDetails.scss';
 
 
-function BookDetails(props){
+function BookDetails(){
     return(
         <>
-        <h1 className= 'heading_style'>Books</h1>
-        {props.BookData.map(function ncard(val){
+        <div className="book_head">
+            <div className="boot_item">
+                <span className="book_heading">Books</span>
+                <div className="book_count">
+                    <div className="count">({Bdata.length}Items)</div>
+                </div>
+            </div>
+            <div className="book-sort">
+                <div className="sort">
+                    <select className="sort-box">
+                        <option selected value="dafault">Sort by relevence</option>
+                        <option value="Price:Low to High">Price:Low to High</option>
+                        <option value="Price:High to Low">Price:High to Low</option>
+                        <option value="Newest Arrival">Newest Arrival</option>
+                    </select>
+
+                </div>
+            </div>
+        </div>
+        {Bdata.map(function ncard(val){
             return(
                 <Card
-                key={val.bookId}
-                imgsrc={val.imageURL}
+                key={val.id}
+                imgsrc={val.imgsrc}
                 authorName={val.authorName}
                 bookName={val.bookName}
-                price={val.bookPrice}
-                changeNumber = {props.change}
+                price={val.price}
                 />
-            )
+             )
         })}
     </>
     )
