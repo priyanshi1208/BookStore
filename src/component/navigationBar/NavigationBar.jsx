3be45d6eefd8ab99d04d2b3657b2fe99ce14709a
Rajnish20 <rajnishsahu95@gmail.com>
2021-08-23 14:47:40 +0530

    Add-Display User Name after logging in

--- src/component/navigationBar/NavigationBar.jsx
+++ src/component/navigationBar/NavigationBar.jsx
@@ -4,8 +4,23 @@ import logo from '../../images/education.svg';
 import search from '../../images/search.png';
 import cart from '../../images/supermarket.svg';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import StoreService from '../../service/StoreService';
 
 function NavigationBar(props){  
+
+    const[userName,setUserName] = useState('');
+
+    useEffect(() => {
+        new StoreService().getUserById(localStorage.getItem("userId"))
+        .then(responseDTO => {
+           let responseData = responseDTO.data;
+           setUserName("Hi, "+responseData.data.name);
+       }).catch(error => {
+           console.log("Error while Fetching Data"+JSON.stringify(error));
+        })
+    },[])
     
     const handleCartClick = () => {
         let id = localStorage.getItem("userId");
@@ -34,6 +49,7 @@ function NavigationBar(props){
                 <img className="cart-img" src={cart} alt="cart" onClick={handleCartClick} ></img>
                 <p className="cart-number">{props.cartNumber}</p>
             </div>  
+            <p className="user-name">{userName}</p>
         </nav>
     )
 }
